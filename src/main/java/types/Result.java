b1390f1b222a0f925eb378f06309cc79ed9d7430
James Richie <james.richie.sulaeman@gmail.com>
2022-12-13 19:35:52 -0800

    Minor refactor

--- src/main/java/types/Result.java
+++ src/main/java/types/Result.java
@@ -1,24 +1,24 @@
 package types;
 
 public class Result {
-    public double total;
-    public int sampleSize;
+    public double totalSuccesses;
+    public int totalSamples;
 
     public Result() {
-        total = 0.0;
-        sampleSize = 0;
+        totalSuccesses = 0.0;
+        totalSamples = 0;
     }
 
-    public Result(double total, int sampleSize) {
-        this.total = total;
-        this.sampleSize = sampleSize;
+    public Result(double totalSuccesses, int totalSamples) {
+        this.totalSuccesses = totalSuccesses;
+        this.totalSamples = totalSamples;
     }
 
     public Result add(Result other) {
-        return new Result(total + other.total, sampleSize + other.sampleSize);
+        return new Result(totalSuccesses + other.totalSuccesses, totalSamples + other.totalSamples);
     }
 
     public String toString() {
-        return "Total: " + total + "\nSample Size: " + sampleSize + "\nAverage: " + (total / sampleSize);
+        return "Total Successes: " + totalSuccesses + "\nSample Size: " + totalSamples + "\nAverage success rate: " + (totalSuccesses / totalSamples);
     }
 }
